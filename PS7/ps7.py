35c6c9895f396e4e8de578285ee27a1e08fd6217
ravitezu <ravi-teja@live.com>
2014-07-27 23:18:23 +0530

    part1 done

--- PS7/ps7.py
+++ PS7/ps7.py
@@ -47,7 +47,31 @@ def process(url):
 # Problem 1
 
 # TODO: NewsStory
+class NewsStory(object):
+    """Creates a NewsStory objects"""
+    def __init__(self, guid, title, subject, summary, link):
+        """Constructor of NewsStory class"""
+        self.guid    = guid
+        self.title   = title
+        self.subject = subject
+        self.summary = summary
+        self.link    = link
 
+    def getGuid(self):
+        return self.guid
+
+    def getTitle(self):
+        return self.title
+
+    def getSubject(self):
+        return self.subject
+   
+    def getSummary(self):
+        return self.summary
+   
+    def getLink(self):
+        return self.link
+    
 #======================
 # Part 2
 # Triggers
