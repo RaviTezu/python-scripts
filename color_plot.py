2fc78ebfa97d4b082906fa4c40419c69ce40010e
RaviTezu <ravi-teja@live.com>
2014-08-21 13:08:21 +0530

    Yikes! More to color_plot

--- color_plot.py
+++ color_plot.py
@@ -2,20 +2,24 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-x = range(1, 31)
-#ax1 = plt.axes(frameon=False)
-p1 = [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,1]
-p2 = [0,2,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,0,2,2,2,0,2]
-p3 = [0,3,0,0,3,0,0,0,0,3,0,0,0,0,0,0,0,3,0,3,0,0,3,3,3,3,0,0,0,3]
-p4 = [0,4,0,4,0,4,0,0,0,0,0,0,4,4,4,4,4,0,4,0,4,0,0,0,0,0,0,4,4,0]
+x = range(0, 30)
+p0 = [0,0,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,0,0,0,0,0,0,0,0,0,0,0,None,0]
+p1 = [None,1,None,None,1,None,None,None,None,None,None,None,None,None,None,None,None,1,1,1,1,1,1,1,None,1,1,1,None,1]
+p2 = [None,2,None,None,2,None,None,None,None,2,None,None,None,None,None,None,None,None,None,None,None,None,2,2,2,2,None,None,None,2]
+p3 = [None,3,None,3,None,3,3,None,3,None,3,None,3,3,3,3,3,None,3,None,3,None,3,None,None,3,None,3,3,None]
+p4 = [None,4,None,4,None,4,None,None,None,None,None,None,4,4,4,None,None,None,None,None,4,None,None,None,None,4,None,None,4,None]
+ylabel  = ["P1", "P2", "P3", "P4", "P5"]
+xlabel  =  range(1,31)
 for xc in x:
     plt.axvline(x=xc)
 #ax1.axes.get_yaxis().set_visible(False) 
+plt.yticks(x, ylabel)
+#plt.scatter(x, p1,color="green",s=100)
+plt.scatter(x, p0,color="red",s=100)
 plt.scatter(x, p1,color="green",s=100)
-plt.scatter(x, p2,color="red",s=100)
-plt.scatter(x, p3,color="blue",s=100)
-plt.scatter(x, p4,color="purple",s=100)
-plt.xticks(np.arange(min(x), max(x)+1, 1.0))
-plt.legend(['p1', 'p2', 'p3', 'p4'], loc='upper left')
+plt.scatter(x, p2,color="blue",s=100)
+plt.scatter(x, p3,color="purple",s=100)
+plt.scatter(x, p4,color="brown",s=100)
+plt.xticks(np.arange(min(x), max(x)+1, 1.0),xlabel)
+#plt.legend(['p1', 'p2', 'p3', 'p4'], loc='upper left')
 plt.show()
-
